89f074ad34b4c9bf3ae80dc97d9c4915e0a8c77b
Fernando Carosi Lourenço <zippetanimations@gmail.com>
2025-04-07 22:23:24 -0300

    ,

---
+++ app.js
@@ -0,0 +1,52 @@
+function fnIdiomaIngles(){
+    document.getElementById("inicial").innerText = "Home"
+    document.getElementById("sobre").innerText = "About Us"
+    document.getElementById("imoveis").innerText = "Properties"
+    document.getElementById("contato").innerText = "Contact"
+    document.getElementById("casa").innerText = "Find Your Dream Home"
+    document.getElementById("textao").innerText = "Explore our wide range of properties and find the perfect home for you."
+    document.getElementById("pesquisa").innertText = "Search Properties"
+    document.getElementById("saibamais").innerText = "Learn More"
+    document.getElementById("contato").innerText = "Get in touch with us today!"
+    document.getElementById("contato2").innerText = "Contact Us"
+    document.getElementById("telefone").innerText = "Phone"
+    document.getElementById("escritorio").innerText = "Office"
+    document.getElementById("subscricao").innerText = "Subscribe to our newsletter for the latest updates on new features and product releases."
+    document.getElementById("cadastrar").innerText = "Subscribe"
+    document.getElementById("direitos").innerText = "© 2025 Guto Xavier. All rights reserved."
+
+}
+
+function fnBanner1(){
+    document.getElementById("banner").src="images/banner01.jpg"
+}
+
+function fnBanner2(){
+    document.getElementById("banner").src="images/banner02.jpg"
+}
+ 
+function fnBanner3(){
+    document.getElementById("banner").src="images/banner03.jpg"
+}
+
+function fnBanner4(){
+    document.getElementById("banner").src="images/banner01.jpg"
+}
+
+function fnIdiomaPortugues(){
+    document.getElementById("inicial").inertText = "Inicial"
+    document.getElementById("sobre").inertText = "Sobre Nós"
+    document.getElementById("imoveis").inertText = "Imóveis"
+    document.getElementById("contato").inertText = "Contato"
+    document.getElementById("casa").inertText = "Encontre a casa dos seus sonhos"
+    document.getElementById("textao").inertText = "Explore nossa ampla variedade de imóveis e encontre a casa perfeita para você."
+    document.getElementById("pesquisa").inertText = "Pesquise Imóveis"
+    document.getElementById("saibamais").inertText = "Saiba Mais"
+    document.getElementById("contato").inertText = "Entre em contato conosco hoje mesmo!!"
+    document.getElementById("contato2").inertText = "Entre em contato"
+    document.getElementById("telefone").inertText = "Telefone"
+    document.getElementById("escritorio").inertText = "Escritório"
+    document.getElementById("subscricao").inertText = "Assine nossa newsletter para receber as últimas atualizações sobre novos recursos e lançamentos de produtos."
+    document.getElementById("cadastrar").inertText = "Cadastrar"
+    document.getElementById("direitos").inertText = "© 2025 Guto Xavier. Todos os direitos reservados."
+}
